bff79abf98883a160feaa0ded08827f9ec6bd454
trofimenko-surf <trofimenko@surfstudio.co>
2019-11-21 18:12:33 +0300

    test

--- ci-internal/DevDeployJobJenkinsfile.groovy
+++ ci-internal/DevDeployJobJenkinsfile.groovy
@@ -109,8 +109,8 @@ pipeline.stages = [
             script.sh("./gradlew writeToFileReleaseNotesDiff -PrevisionToCompare=${mainBranchHeadHash}")
             String releaseNotesChanges = script.readFile(releaseNotesChangesFileUrl)
 
-            def message = releaseNotesChanges
-            JarvisUtil.sendMessageToGroup(script, message, idChatAndroidSlack, "slack", true)
+            script.echo "1111 ${releaseNotesChanges}"
+            JarvisUtil.sendMessageToGroup(script, releaseNotesChanges, idChatAndroidSlack, "slack", true)
         },
         pipeline.stage(CHECK_BRANCH_AND_VERSION) {
             String globalConfigurationJsonStr = script.readFile(projectConfigurationFile)
