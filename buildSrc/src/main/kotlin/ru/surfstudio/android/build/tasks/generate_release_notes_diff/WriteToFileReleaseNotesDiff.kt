61d2d6ea0308f2084d085282fec8171e3eb6bf75
trofimenko <trofimenko@surfstudio.co>
2019-11-25 11:02:58 +0300

    test6

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/generate_release_notes_diff/WriteToFileReleaseNotesDiff.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/generate_release_notes_diff/WriteToFileReleaseNotesDiff.kt
@@ -74,7 +74,7 @@ open class WriteToFileReleaseNotesDiff : DefaultTask() {
 
         var i = 1
         val sr = StandardRepository()
-        val branchName = sr.getCommit("22a6e74cff9abad3492909686519c74f851c03e8")
+        val branchName = sr.getCommit("d6b9b70451e466110258c7a3bc65ac26a067fb0c")
                 .parents.forEach {
 
             releaseNotesChangesFile.appendText("${i++} ${it.name}\n")
